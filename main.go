7c22683d32c79feff7614d31554863a94d5d9f95
Bonnie Simon <bonnie.developer@gmail.com>
2024-07-14 21:11:20 +0530

    Add module and got it working

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "file-sharing/helper"
+)
+
+func main() {
+    result := helper.Add(2, 3) // Call the Add function from the helper package
+    fmt.Println(result)
+}
