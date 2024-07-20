b307b263366af8ff3b9580ab238cd0a860235a87
Bonnie Simon <bonnie.developer@gmail.com>
2024-07-21 01:02:39 +0530

    Add loop for receiving connections

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	address := "127.0.0.1:3005"
+	const address string = ":3005"
+
 	listener, err := net.Listen("tcp", address)
 
 	if err != nil {
@@ -16,16 +17,22 @@ func main() {
 	}
 	defer listener.Close()
 
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
+	for {
+		fmt.Printf("Server ready to accept connections to %s\n", address)
+
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fatal error: %s\n", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Printf("Connected to %s\n", address)
+
+		buf := make([]byte, 256)
+
+		n, _ := conn.Read(buf)
+
+		fmt.Printf("Read %d bytes\n", n)
+		fmt.Printf("read the following data: %s\n", string(buf))
 	}
-
-	buf := make([]byte, 256)
-
-	n, _ := conn.Read(buf)
-
-	fmt.Printf("Read %d bytes\n", n)
-	fmt.Printf("read the following data: %s\n", string(buf))
 }
